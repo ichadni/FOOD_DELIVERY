8a2cf64067c5bd56f27009aa7bed9ab31c7a7547
ichadni <156695216+ichadni@users.noreply.github.com>
2026-01-03 04:51:04 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Login from './screens/Login'
+
+import Home from './screens/Home'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/login' element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
